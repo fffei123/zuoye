ea89853140cab9662bcda5ce1346e225a28c3308
LI22503354 <2198652794@qq.com>
2026-05-14 00:20:07 +0800

    change location code anf debug

--- zuoye/Pages/NearbyBinPage.xaml.cs
+++ zuoye/Pages/NearbyBinPage.xaml.cs
@@ -1,17 +1,55 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui.Devices.Sensors;
+using Microsoft.Maui.ApplicationModel;
+using System.Collections.ObjectModel;
 
-namespace zuoye
+namespace zuoye;
+
+public partial class NearbyBinPage : ContentPage
 {
-    public partial class NearbyBinPage : ContentPage
-    {
-        public NearbyBinPage()
-        {
-            InitializeComponent();
-        }
+    public ObservableCollection<string> BinList { get; set; }
 
-        private async void GoBackHome(object sender, EventArgs e)
+    public NearbyBinPage()
+    {
+        InitializeComponent();
+
+        BinList = new ObservableCollection<string>
         {
-            await Navigation.PopToRootAsync();
+            "♻️ Recyclable Bin - 100m",
+            "☠️ Hazardous Bin - 150m",
+            "🗑️ General Waste Bin - 300m"
+        };
+        BindingContext = this;
+
+        _ = GetGPS();
+    }
+
+    private async Task GetGPS()
+    {
+        try
+        {
+            var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+            {
+                lblGPS.Text = "Location permission denied";
+                return;
+            }
+
+            var req = new GeolocationRequest(GeolocationAccuracy.Low, TimeSpan.FromSeconds(8));
+            var loc = await Geolocation.Default.GetLocationAsync(req);
+
+            if (loc != null)
+                lblGPS.Text = $"Current GPS Location: {loc.Latitude:F4}, {loc.Longitude:F4}";
+            else
+                lblGPS.Text = "Location unavailable";
         }
+        catch
+        {
+            lblGPS.Text = "Location error";
+        }
+    }
+
+    private async void GoBackHome(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
     }
 }
